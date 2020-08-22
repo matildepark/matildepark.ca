2fe77f44dfcb974f7c6a9c1f26ef2059bd57f718
Matilde Park <matilde@tlon.io>
2020-08-22 00:46:27 -0400

    build.sh: interim pandoc dir

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-mkdir site
-mkdir site/about
-mkdir site/cv
-mkdir site/posts
-mkdir site/projects
+mkdir temp
+mkdir temp/about
+mkdir temp/cv
+mkdir temp/posts
+mkdir temp/projects
 
 cd posts
-for f in *.md; do pandoc -f markdown -t html "$f" -o "../site/posts/${f%.md}.html";done
+for f in *.md; do pandoc -f markdown -t html "$f" -o "../temp/posts/${f%.md}.html";done
 
 cd ../about
-pandoc -f markdown -t html index.md -o ../site/about/index.html
+pandoc -f markdown -t html index.md -o ../temp/about/index.html
 
 cd ../cv
-pandoc -f markdown -t html index.md -o ../site/cv/index.html
+pandoc -f markdown -t html index.md -o ../temp/cv/index.html
 
 cd ..
+
+mkdir site
+
+cp -R temp/ site
 
 cc -std=c99 -DDEBUG -Wall -Wpedantic -Wshadow -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -g -Og -fsanitize=address -fsanitize=undefined main.c -o main
 
@@ -25,3 +29,5 @@ rm ./main
 cp -R assets site
 cp -R img site
 cp index.html CNAME site
+
+rm -rf temp
